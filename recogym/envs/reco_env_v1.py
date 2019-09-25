c9bb8092c994896a17e5037285a1c4ba937b8c9f
David Rohde <d.rohde@criteo.com>
2019-09-25 16:31:10 +0200

    speed up random choice by Olivier J

--- recogym/envs/reco_env_v1.py
+++ recogym/envs/reco_env_v1.py
@@ -8,8 +8,8 @@
 # Beta is the latent representation of response to actions (matrix P by K)
 # sigmoid(beta omega) is the ctr for each action
 
-from numpy import array, diag, exp, matmul, mod, sqrt
-from scipy.special import expit as sigmoid
+from numpy import array, diag, exp, matmul, mod
+#from scipy.special import expit as sigmoid
 from .abstract import AbstractEnv, env_args, organic
 
 # Default arguments for toy environment ------------------------------------
@@ -24,16 +24,53 @@ env_1_args = {
         'number_of_flips': 0,
         'sigma_mu_organic': 3,
         'change_omega_for_bandits': False,
-        'normalize_beta': False
     }
 }
 
+########## EXPERIMENTAL ##############
+import numpy as np
+from numba import jit
+@jit(nopython=True)
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def fast_choice(n_options, probs, rng):
+    # Numpy.random.choice is fast when vectorised, but we call it for single choices
+    # This very simple algorithm is faster for a small number of options (empirically - < 150)
+    # Generate a random number
+    pchoice = rng.random_sample()
+    # Get the probablity for the first option
+    running_sum = probs[0]
+    running_choice = 0
+    # Loop over options
+    for p in probs[1:]:
+        # If our random number is bigger than the sum of preceding probabilities
+        # Pick this one
+        if pchoice <= running_sum:
+            break
+        else:
+            running_sum += p
+            running_choice += 1
+    # Machine precision issues make that the probabilities sometimes do not sum exactly to one
+    # If this becomes an issue, divide the remaining probability mass over all items
+    if running_choice >= n_options:
+        return fast_choice(n_options, [1/n_options]*n_options, rng)
+    return running_choice
+
+@jit(nopython=True)
+def numba_fast_choice(probs, p):
+    return np.searchsorted(probs.cumsum(),p)
+##########/EXPERIMENTAL ##############
 
 # Maps behaviour into ctr - organic has real support ctr is on [0,1].
+@jit(nopython=True)
+def compute_ctr(beta, omega, mu_bandit):
+    return ff(np.dot(beta, omega)[:, 0] + mu_bandit)
+
+@jit(nopython=True)
 def ff(xx, aa = 5, bb = 2, cc = 0.3, dd = 2, ee = 6):
     # Magic numbers give a reasonable ctr of around 2%.
     return sigmoid(aa * sigmoid(bb * sigmoid(cc * xx) - dd) - ee)
-
 
 # Environment definition.
 class RecoEnv1(AbstractEnv):
@@ -76,7 +113,8 @@ class RecoEnv1(AbstractEnv):
 
     # Update user state to one of (organic, bandit, leave) and their omega (latent factor).
     def update_state(self):
-        self.state = self.rng.choice(3, p = self.state_transition[self.state, :])
+        #self.state = self.rng.choice(3, p = self.state_transition[self.state, :])
+        self.state = fast_choice(3, self.state_transition[self.state, :], self.rng)
         assert (hasattr(self, 'time_generator'))
         old_time = self.current_time
         self.current_time = self.time_generator.new_time()
@@ -94,37 +132,46 @@ class RecoEnv1(AbstractEnv):
     # click ~ Bernoulli().
     def draw_click(self, recommendation):
         # Personalised CTR for every recommended product.
-        ctr = ff(matmul(self.beta, self.omega)[:, 0] + self.mu_bandit)
-        click = self.rng.choice(
-            [0, 1],
-            p = [1 - ctr[recommendation], ctr[recommendation]]
-        )
-        return click
+        #ctr = ff(np.matmul(self.beta, self.omega)[:, 0] + self.mu_bandit)
+        ctr = compute_ctr(self.beta, self.omega, self.mu_bandit)
+        #click = self.rng.choice(
+        #    [0, 1],
+        #    p = [1 - ctr[recommendation], ctr[recommendation]]
+        #)
+        #return click
+        pchoice = self.rng.uniform(0,1)
+        if pchoice <= ctr[recommendation]:
+            return 1
+        return 0
+
+    ## EXPERIMENTAL ##
+    def _get_true_pclick(self):
+        return compute_ctr(self.beta, self.omega, self.mu_bandit)
+    ##/EXPERIMENTAL/##
 
     # Sample the next organic product view.
     def update_product_view(self):
         log_uprob = matmul(self.Gamma, self.omega)[:, 0] + self.mu_organic
         log_uprob = log_uprob - max(log_uprob)
         uprob = exp(log_uprob)
-        self.product_view = int(
-            self.rng.choice(
-                self.config.num_products,
-                p = uprob / sum(uprob)
-            )
-        )
-
-    def normalize_beta(self):
-        self.beta = self.beta / sqrt((self.beta**2).sum(1)[:,None])
+        
+        # Sample fast
+        self.product_view = numba_fast_choice(uprob/sum(uprob),self.rng.random_sample())
+        #if self.config.num_products > 150:
+        #    self.product_view = int(
+        #        self.rng.choice(
+        #            self.config.num_products,
+        #            p = uprob / sum(uprob)
+        #        )
+        #    )
+        #else:
+        #    self.product_view = fast_choice(self.config.num_products, uprob/sum(uprob), self.rng)
 
     def generate_beta(self, number_of_flips):
         """Create Beta by flipping Gamma, but flips are between similar items only"""
-
         if number_of_flips == 0:
             self.beta = self.Gamma
             self.mu_bandit = self.mu_organic
-            if self.config.normalize_beta:
-                self.normalize_beta()
-
             return
         P, K = self.Gamma.shape
         index = list(range(P))
@@ -151,12 +198,7 @@ class RecoEnv1(AbstractEnv):
                 if flips == number_of_flips:
                     self.beta = self.Gamma[index, :]
                     self.mu_bandit = self.mu_organic[index]
-                    if self.config.normalize_beta:
-                        self.normalize_beta()
                     return
 
         self.beta = self.Gamma[index, :]
         self.mu_bandit = self.mu_organic[index]
-
-        if self.config.normalize_beta:
-            self.normalize_beta()
