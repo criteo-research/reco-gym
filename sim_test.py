44c1f98342869035fdb17ecb900d7e3f8b0a4979
David Rohde <d.rohde@criteo.com>
2019-09-27 19:57:13 +0200

    running defaults changed

--- sim_test.py
+++ sim_test.py
@@ -1,3 +1,4 @@
+import datetime
 import argparse
 import imp
 import os
@@ -11,11 +12,11 @@ from recogym import (
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--P', type = int, default = 100, help = 'Number of products')
-parser.add_argument('--U', type = int, default = 100, help = 'Number of users to train on')
-parser.add_argument('--Utest', type = int, default = 1000, help = 'Number of users to test')
-parser.add_argument('--seed', type = int, default = 100, help = 'Seed')
+parser.add_argument('--U', type = int, default = 200, help = 'Number of users to train on')
+parser.add_argument('--Utest', type = int, default = 2000, help = 'Number of users to test')
+parser.add_argument('--seed', type = int, default = 42, help = 'Seed')
 parser.add_argument('--K', type = int, default = 20, help = 'Number of latent factors')
-parser.add_argument('--F', type = int, default = 20,
+parser.add_argument('--F', type = int, default = 50,
                     help = 'Number of flips, how different is bandit from organic')
 parser.add_argument('--log_epsilon', type = float, default = 0.05,
                     help = 'Pop logging policy epsilon')
@@ -35,8 +36,9 @@ for e in os.listdir(entries_dir):
     print(open(entries_dir + '/' + e).read())
 
     tmp_module = imp.new_module('tmp_module')
+    tmp_module.__dict__['P']=P
     exec(open(entries_dir + '/' + e).read(), tmp_module.__dict__,)
-    agent_key = tmp_module.agent.keys()[0]
+    agent_key = list(tmp_module.agent.keys())[0]
     agent_data = tmp_module.agent[agent_key]
     df = competition_score(
         P,
@@ -59,12 +61,12 @@ for e in os.listdir(entries_dir):
 
     adf.append(df)
 
-out_dir = entries_dir + '_' + str(P) + '_' + str(U) + '_' + str(Utest) + '_' + str(start)
+out_dir = entries_dir + '_' + str(P) + '_' + str(U) + '_' + str(Utest) + '_' + str(datetime.datetime.now())
 os.mkdir(out_dir)
 fp = open(out_dir + '/config.txt', 'w')
 fp.write(str(args))
 fp.close()
 
 leaderboard = pd.concat(adf)
-leaderboard = leaderboard.sort_values(by = 'q0.5', ascending = False)
+leaderboard = leaderboard.sort_values(by = 'q0.500', ascending = False)
 leaderboard.to_csv(out_dir + '/leaderboard.csv')
