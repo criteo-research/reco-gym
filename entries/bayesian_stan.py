964496e806da59d3b93a2f74d9528db1e0dddef3
David Rohde <d.rohde@criteo.com>
2019-10-01 12:04:09 +0200

    too slow

--- entries/bayesian_stan.py
+++
@@ -1,6 +0,0 @@
-from recogym.agents import BayesianAgent, bayesian_poly_args
-bayesian_poly_args['aa'] = 0.001
-bayesian_poly_args['bb'] = 0.001
-pytorch_banditnet_args['num_products'] = P
-
-agent = build_agent_init('BayesianAgent', BayesianAgent, {**bayesian_poly_args})
