c4b10dc95e636febb48722ae4f9d52734b24c6fb
Dmytro Mykhaylov <d.mykhaylov@criteo.com>
2019-10-01 16:59:00 +0200

    new version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="recogym",
-    version="0.1.1.1",
+    version="0.1.1.2",
     description="Open-AI gym reinforcement learning environment for recommendation",
     long_description=README,
     long_description_content_type="text/markdown",
