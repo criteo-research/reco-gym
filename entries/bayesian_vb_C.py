fc4fa58429a0c0570a1d93c60fb770d939d7b6c8
David Rohde <d.rohde@criteo.com>
2019-10-01 12:02:37 +0200

    rm

--- entries/bayesian_vb_C.py
+++
@@ -1,6 +0,0 @@
-from recogym.agents import BayesianAgentVB, bayesian_poly_args
-bayesian_poly_args['aa'] = 0.2
-bayesian_poly_args['bb'] = 0.1
-pytorch_banditnet_args['num_products'] = P
-
-agent = build_agent_init('BayesianAgentVB', BayesianAgentVB, {**bayesian_poly_args})
